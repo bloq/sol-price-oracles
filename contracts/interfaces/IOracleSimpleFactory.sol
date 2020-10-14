2cb22b86be02e83f5f9e6c0dd66f63fddf77db74
Jeff Garzik <jgarzik@localhost.localdomain>
2020-10-14 20:48:18 +0000

    OracleFactory: add isOurs ABI, to match other factories

--- contracts/interfaces/IOracleSimpleFactory.sol
+++ contracts/interfaces/IOracleSimpleFactory.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.6.6;
 interface IOracleSimpleFactory {
     event OracleCreated(address indexed _sender, address indexed _newOracle);
 
+    function ours(address a) external view returns (bool);
     function oracleCount() external view returns (uint);
     function oracleAt(uint idx) external view returns (address);
     function getOracle(address tokenA, address tokenB, uint _period) external view returns (address);
